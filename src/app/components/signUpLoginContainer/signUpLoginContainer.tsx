a73b3dd5ee46536250c8c3301e184cf7e1d18440
Sandushan8 <dewpurai@gmail.com>
2024-08-07 11:57:07 +0530

    Refactor tailwind css

--- src/app/components/signUpLoginContainer/signUpLoginContainer.tsx
+++ src/app/components/signUpLoginContainer/signUpLoginContainer.tsx
@@ -1,27 +1,27 @@
-"use client";
-import Image from "next/image";
-import { Input, Button, Checkbox } from "@nextui-org/react";
+'use client';
+import Image from 'next/image';
+import { Input, Button, Checkbox } from '@nextui-org/react';
 import {
   containerTypes,
   SignUpLoginContainerProps,
   submitData,
-} from "./signUpLoginContainer.types";
-import Link from "next/link";
-import { useState } from "react";
-import { EyeSlashFilledIcon } from "./EyeSlashed";
-import { EyeFilledIcon } from "./EyeFilled";
-import { cookies } from "next/headers";
-import { storeCookies } from "./signUpLoginContainer.utils";
+} from './signUpLoginContainer.types';
+import Link from 'next/link';
+import { useState } from 'react';
+import { EyeSlashFilledIcon } from './EyeSlashed';
+import { EyeFilledIcon } from './EyeFilled';
+import { cookies } from 'next/headers';
+import { storeCookies } from './signUpLoginContainer.utils';
 
 export default function SignUpLoginContainer({
   type,
   imageUrl,
 }: SignUpLoginContainerProps) {
   const [isVisible, setIsVisible] = useState(false);
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [isChecked, setIsChecked] = useState(false);
-  const [password, setPassword] = useState("");
-  const [fullName, setFullName] = useState("");
+  const [password, setPassword] = useState('');
+  const [fullName, setFullName] = useState('');
 
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
@@ -34,7 +34,7 @@ export default function SignUpLoginContainer({
 
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="hidden md:block md:relative w-screen h-1/2 md:w-1/2 md:h-screen overflow-hidden">
+      <div className="hidden h-1/2 w-screen overflow-hidden md:relative md:block md:h-screen md:w-1/2">
         <Image
           src={imageUrl}
           alt="Logo"
@@ -43,29 +43,29 @@ export default function SignUpLoginContainer({
           className="md:rounded-br-3xl"
         />
       </div>
-      <div className="z-10 w-screen md:w-1/2 h-full pb-10 rounded-b-2xl md:rounded-none shadow-2xl md:shadow-none md:px-40 pt-20 px-2 md:pt-40 bg-white">
+      <div className="z-10 h-full w-screen rounded-b-2xl bg-white px-2 pb-10 pt-20 shadow-2xl md:w-1/2 md:rounded-none md:px-40 md:pt-40 md:shadow-none">
         <div>
           <h1
-            className="text-4xl md:text-header text-center md:text-left "
+            className="text-center text-4xl md:text-left md:text-header"
             style={{
-              color: type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+              color: type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
-            {type === containerTypes.login ? "Login" : "Sign up"}
+            {type === containerTypes.login ? 'Login' : 'Sign up'}
           </h1>
           <div className="flex justify-center md:justify-normal">
-            <p className="text-sm text-center md:text-left w-44 md:w-full md:text-normal mt-2 md:mt-5">
-              {"Please enter your login details to start having fun!"}
+            <p className="mt-2 w-44 text-center text-sm md:mt-5 md:w-full md:text-left md:text-normal">
+              {'Please enter your login details to start having fun!'}
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap-2 md:gap-5 mt-4 mb-4">
+        <div className="mb-4 mt-4 flex flex-col gap-2 md:gap-5">
           {type === containerTypes.signup && (
             <Input
               variant="bordered"
               type="text"
               label="Full Name"
-              placeholder={"Baba Tunde"}
+              placeholder={'Baba Tunde'}
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
             />
@@ -74,14 +74,14 @@ export default function SignUpLoginContainer({
             variant="bordered"
             type="email"
             label="Email Address"
-            placeholder={"info@example.com"}
+            placeholder={'info@example.com'}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             variant="bordered"
-            type={isVisible ? "text" : "password"}
-            placeholder={"password"}
+            type={isVisible ? 'text' : 'password'}
+            placeholder={'password'}
             size="lg"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -93,9 +93,9 @@ export default function SignUpLoginContainer({
                 aria-label="toggle password visibility"
               >
                 {isVisible ? (
-                  <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  <EyeFilledIcon className="pointer-events-none text-2xl text-default-400" />
                 ) : (
-                  <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  <EyeSlashFilledIcon className="pointer-events-none text-2xl text-default-400" />
                 )}
               </button>
             }
@@ -105,20 +105,20 @@ export default function SignUpLoginContainer({
           <div className="flex flex-row">
             <Checkbox
               isSelected={isChecked}
-              color={type === containerTypes.login ? "secondary" : "success"}
+              color={type === containerTypes.login ? 'secondary' : 'success'}
               onValueChange={setIsChecked}
             />
             {type === containerTypes.login ? (
               <p className="text-sm md:text-normal">Keep me logged in</p>
             ) : (
               <p className="text-sm md:text-normal">
-                I agree to the{" "}
-                <b style={{ color: "#165A0F" }}>Terms & Conditions</b>
+                I agree to the{' '}
+                <b style={{ color: '#165A0F' }}>Terms & Conditions</b>
               </p>
             )}
           </div>
           {type === containerTypes.login && (
-            <div className={"text-sm md:text-normal text-loginMain font-bold"}>
+            <div className={'text-sm font-bold text-loginMain md:text-normal'}>
               Forgot password?
             </div>
           )}
@@ -126,85 +126,85 @@ export default function SignUpLoginContainer({
         <div>
           <Button
             color="primary"
-            className={"w-full mt-2 mb-2 md:mt-5 md:mb-5"}
+            className={'mb-2 mt-2 w-full md:mb-5 md:mt-5'}
             style={{
-              fontSize: "25px",
-              height: "60px",
+              fontSize: '25px',
+              height: '60px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
             onClick={() => {
               if (
                 type === containerTypes.login &&
-                (email === "" || password === "")
+                (email === '' || password === '')
               ) {
-                alert("Please fill in all fields");
+                alert('Please fill in all fields');
                 return;
               }
               if (
-                (type === containerTypes.signup && fullName === "") ||
-                email === "" ||
-                password === ""
+                (type === containerTypes.signup && fullName === '') ||
+                email === '' ||
+                password === ''
               ) {
-                alert("Please fill in all fields");
+                alert('Please fill in all fields');
                 return;
               }
               submit({ email, password, fullName });
             }}
           >
-            {type === containerTypes.login ? "Log in" : "Sign up"}
+            {type === containerTypes.login ? 'Log in' : 'Sign up'}
           </Button>
         </div>
-        <div className="text-sm md:text-normal mb-3 md:mb-8 flex gap-5">
+        <div className="mb-3 flex gap-5 text-sm md:mb-8 md:text-normal">
           {type === containerTypes.login
             ? "Don't have an account?"
-            : "Already have an account?"}
+            : 'Already have an account?'}
           <Link
-            href={type === containerTypes.login ? "/signup" : "/login"}
+            href={type === containerTypes.login ? '/signup' : '/login'}
             style={{
-              color: type === containerTypes.login ? "#3C1AB9" : "#165A0F",
-              fontWeight: "bold",
+              color: type === containerTypes.login ? '#3C1AB9' : '#165A0F',
+              fontWeight: 'bold',
             }}
           >
-            {type === containerTypes.login ? "Sign Up" : "Login"}
+            {type === containerTypes.login ? 'Sign Up' : 'Login'}
           </Link>
         </div>
-        <div className="flex justify-center text-sm md:text-normal w-full flex justify gap-3 mb-3 md:mb-10">
+        <div className="justify mb-3 flex w-full justify-center gap-3 text-sm md:mb-10 md:text-normal">
           <div
             className="flex"
             style={{
-              alignItems: "center",
+              alignItems: 'center',
             }}
           >
-            <div className="bg-black h-0.5 w-smallLineWidth md:w-lineWidth" />
+            <div className="h-0.5 w-smallLineWidth bg-black md:w-lineWidth" />
           </div>
           Or continue with
           <div
             className="flex"
             style={{
-              alignItems: "center",
+              alignItems: 'center',
             }}
           >
-            <div className="bg-black h-0.5 w-smallLineWidth md:w-lineWidth" />
+            <div className="h-0.5 w-smallLineWidth bg-black md:w-lineWidth" />
           </div>
         </div>
         <div
-          className="flex justify-between hidden md:flex"
+          className="hidden justify-between md:flex"
           style={{
-            justifyContent: "space-between",
+            justifyContent: 'space-between',
           }}
         >
           <Button
             color="success"
             style={{
-              width: "105px",
-              height: "80px",
+              width: '105px',
+              height: '80px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/twitter.png"}
+              src={'/icons/twitter.png'}
               alt="twitter"
               width={56}
               height={56}
@@ -213,14 +213,14 @@ export default function SignUpLoginContainer({
           <Button
             color="success"
             style={{
-              width: "105px",
-              height: "80px",
+              width: '105px',
+              height: '80px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/facebook.png"}
+              src={'/icons/facebook.png'}
               alt="facebook"
               width={46}
               height={46}
@@ -229,14 +229,14 @@ export default function SignUpLoginContainer({
           <Button
             color="success"
             style={{
-              width: "105px",
-              height: "80px",
+              width: '105px',
+              height: '80px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/google.png"}
+              src={'/icons/google.png'}
               alt="google"
               width={44}
               height={44}
@@ -245,14 +245,14 @@ export default function SignUpLoginContainer({
           <Button
             color="success"
             style={{
-              width: "105px",
-              height: "80px",
+              width: '105px',
+              height: '80px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/apple.png"}
+              src={'/icons/apple.png'}
               alt="apple"
               width={38}
               height={51}
@@ -263,18 +263,18 @@ export default function SignUpLoginContainer({
           <button
             color="success"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "8px",
-              width: "60px",
-              height: "50px",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '8px',
+              width: '60px',
+              height: '50px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/twitter.png"}
+              src={'/icons/twitter.png'}
               alt="twitter"
               width={20}
               height={20}
@@ -283,18 +283,18 @@ export default function SignUpLoginContainer({
           <button
             color="success"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "8px",
-              width: "60px",
-              height: "50px",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '8px',
+              width: '60px',
+              height: '50px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/facebook.png"}
+              src={'/icons/facebook.png'}
               alt="facebook"
               width={20}
               height={20}
@@ -303,18 +303,18 @@ export default function SignUpLoginContainer({
           <button
             color="success"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "8px",
-              width: "60px",
-              height: "50px",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '8px',
+              width: '60px',
+              height: '50px',
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/google.png"}
+              src={'/icons/google.png'}
               alt="google"
               width={20}
               height={20}
@@ -323,19 +323,19 @@ export default function SignUpLoginContainer({
           <button
             color="success"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "8px",
-              width: "60px",
-              height: "50px",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '8px',
+              width: '60px',
+              height: '50px',
 
               backgroundColor:
-                type === containerTypes.login ? "#3C1AB9" : "#165A0F",
+                type === containerTypes.login ? '#3C1AB9' : '#165A0F',
             }}
           >
             <Image
-              src={"/icons/apple.png"}
+              src={'/icons/apple.png'}
               alt="apple"
               width={20}
               height={20}
@@ -343,7 +343,7 @@ export default function SignUpLoginContainer({
           </button>
         </div>
       </div>
-      <div className="z-0 md:hidden w-screen h-1/2">
+      <div className="z-0 h-1/2 w-screen md:hidden">
         <Image
           src={imageUrl}
           alt="Logo"
